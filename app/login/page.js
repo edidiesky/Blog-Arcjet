08fec9651d80864ce93a9a9bce21d7e28c506620
edidiesky <essienedidiong1000@gmail.com>
2024-06-29 06:59:11 -0700

    new

--- app/login/page.js
+++
@@ -1,9 +0,0 @@
-import Head from "next/head";
-import HomeIndex from "./_components";
-export default async function Root() {
-  return (
-    <div className="relative">
-      <HomeIndex />
-    </div>
-  );
-}
